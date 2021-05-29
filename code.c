d83c8259648665f93afbb800c1fe38e2768c075f
Mohamed Ahmed <mohammed.ahmed.m.228@gmail.com>
2021-05-29 19:42:26 +0200

    phase 1 test case

--- code.c
+++ code.c
@@ -1,9 +1,9 @@
 int x = 0;
 int y;
+int f;
 int r;
-vo(3,4);
 if (x == y){
-    f = f+1;
+    f = f*1;
     {
         r = q;
     }
@@ -13,5 +13,66 @@ if (x == y){
 void vo (int y, int r){
     int x;
     y = r;
+    int counter = 0;
+    const int c = 3;
+
+    for (x = 1; x < 5; x = x + 1){
+        counter = counter + c;
+    }
+
+    bool test = false;
+    if ((counter < 12 && counter >9) || (counter < 20 && counter >16)){
+        test = true;
+    }
+
+    while(x<20){
+        x = x+1;
+    }
+
+    bool b = false;
+    do {
+        b = true;
+    } while(5 == 4);
+
+
 }
+vo(3,4);
+
+
+string name(int num){
+    string n;
+    switch (num){
+        case 1:
+            n =  "Mohamed Ahmed";
+            break;
+        case 2:
+            n =  "Mohamed Shawky";
+            break;
+        case 3:
+            n =  "Ahmed ElKarashily";
+            break;
+        case 4:
+            n =  "Monda Talaat";
+            break;
+        default:
+            n = "ANONYMOUS";
+    }
+    return n;
+}
+
+name();
+
+
+{
+    int x = 0;
+    {
+        int x = 1;
+        {
+            int x = 2;
+            x = x+1;
+        }
+    }
+}
+
+
 
